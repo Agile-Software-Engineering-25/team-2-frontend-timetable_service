223429438ec015cb49ebda4e3c7ad55178596609
abishan.rudrajit <abishan.rudrajit@telekom.de>
2025-10-04 21:15:25 +0200

    Mini Kalender Design verändert

--- src/components/AdministrationNavbar/CalendarMini.tsx
+++ src/components/AdministrationNavbar/CalendarMini.tsx
@@ -13,47 +13,50 @@ type Props = {
 export default function CalendarMini({ date, onChange }: Props) {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
-   <Box
-  sx={{
-    bgcolor: "#eff4f9ff",
-    borderRadius: 2,
-    boxShadow: 1,
-    p: 0.5,
-    "& .MuiDateCalendar-root": {
-      width: "100%",
-      maxHeight: 240,
-    },
-    "& .MuiPickersCalendarHeader-label": {
-      fontWeight: "bold",
-      fontSize: "1rem",
-      textAlign: "center",
-    },
-    "& .MuiDayCalendar-weekDayLabel": {
-      fontWeight: 600,
-      color: "#004080",
-      textAlign: "center",
-      width: "calc(100% / 7)",
-      display: "inline-block",
-      margin: 0,
-      padding: 0,
-      lineHeight: 1,
-      fontSize: "0.7rem",
-    },
-    "& .MuiDayCalendar-weekContainer": {
-      marginTop: 0,
-      paddingTop: 0,
-    },
-    "& .MuiPickersDay-root": {
-      fontSize: "0.5rem",
-      width: "calc(100% / 7)",
-      height: 22,
-      margin: 0,
-      fontStyle: "normal",           // entfernt Kursivschrift
-      fontFamily: "Arial, sans-serif"
-    },
-  }}
->
-
+      <Box
+        sx={{
+          bgcolor: "#eff4f9ff",
+          borderRadius: 0, // <-- Container jetzt eckig
+          boxShadow: 1,
+          p: 0.5,
+          "& .MuiDateCalendar-root": {
+            width: "100%",
+            maxHeight: 240,
+          },
+          "& .MuiPickersCalendarHeader-label": {
+            fontWeight: "bold",
+            fontSize: "1rem",
+            textAlign: "center",
+          },
+          "& .MuiDayCalendar-weekDayLabel": {
+            fontWeight: 600,
+            color: "#004080",
+            textAlign: "center",
+            width: "calc(100% / 7)",
+            display: "inline-block",
+            margin: 0,
+            padding: 0,
+            lineHeight: 1,
+            fontSize: "0.7rem",
+          },
+          "& .MuiDayCalendar-weekContainer": {
+            marginTop: 0,
+            paddingTop: 0,
+          },
+          "& .MuiPickersDay-root": {
+            fontSize: "0.75rem",
+            width: "calc(100% / 7)",
+            height: 28,
+            margin: 0,
+            fontStyle: "normal",
+            fontFamily: "Arial, sans-serif",
+            borderRadius: 0, // <-- Tage auch eckig
+          },
+          "& .Mui-selected": {
+            borderRadius: 0, // <-- Markierung eckig statt rund
+          },
+        }}
+      >
         <DateCalendar
           value={date}
           onChange={onChange}
