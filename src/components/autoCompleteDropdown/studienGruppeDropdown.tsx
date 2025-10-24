386cf975aacaf8b94d8da99f63f1066997ff0f34
Marcel Ahnert <marcel.ahnert@stud-provadis-hochschule.de>
2025-10-24 17:28:27 +0200

    Refactor `StudienGruppeDropdown` to simplify state type, adjust group mapping logic, and remove unused Autocomplete props

--- src/components/autoCompleteDropdown/studienGruppeDropdown.tsx
+++ src/components/autoCompleteDropdown/studienGruppeDropdown.tsx
@@ -7,12 +7,12 @@ import { getGroups } from '@/api/getGroups.ts';
 
 export default function StudienGruppeDropdown() {
   const { formState, updateField } = useFormContext();
-  const [studyGroups, setStudyGroups] = useState<Group[] | null>(null);
+  const [studyGroups, setStudyGroups] = useState<string[] | null>(null);
   useEffect(() => {
     let ignoreResult = false;
     getGroups().then((result) => {
       if (ignoreResult) return;
-      const groups = result.groups.map((group: any ) => {return {name: group.name, id: group.id }});
+      const groups = result.groups.map((group: any ) => {return  group.name });
       setStudyGroups(groups);
     })
     return () => {
@@ -26,8 +26,6 @@ export default function StudienGruppeDropdown() {
         options={studyGroups ?? []}
         value={formState.studienGruppe}
         onChange={(_, value) => {updateField('studienGruppe', value);}}
-        getOptionLabel={(option) => option.name}
-        isOptionEqualToValue={(option, value) => option.id === value.id}
         // Live-Filter beim Tippen (Standard), fallunabhängig
         autoHighlight
         renderInput={(params) => (
