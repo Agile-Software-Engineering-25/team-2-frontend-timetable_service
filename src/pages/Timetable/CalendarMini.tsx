63cecfc15592349c837b41b9cf118cd3acf29577
Marcel Ahnert <marcel.ahnert@stud-provadis-hochschule.de>
2025-10-30 08:35:42 +0100

    Standardize quotes in `CalendarMini` component and improve accessibility with updated `aria-label` attribute.

--- src/pages/Timetable/CalendarMini.tsx
+++ src/pages/Timetable/CalendarMini.tsx
@@ -1,9 +1,9 @@
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { de } from "date-fns/locale";
-import { Box } from "@mui/material";
-import { format } from "date-fns";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { de } from 'date-fns/locale';
+import { Box } from '@mui/material';
+import { format } from 'date-fns';
 
 type Props = {
   date: Date | null;
@@ -13,60 +13,57 @@ type Props = {
 export default function CalendarMini({ date, onChange }: Props) {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
-   <Box
-  sx={{
-    bgcolor: "#eff4f9ff",
-    borderRadius: 2,
-    boxShadow: 1,
-    p: 0.5,
-    "& .MuiDateCalendar-root": {
-      width: "100%",
-      maxHeight: 240,
-    },
-    "& .MuiPickersCalendarHeader-label": {
-      fontWeight: "bold",
-      fontSize: "1rem",
-      textAlign: "center",
-    },
-    "& .MuiDayCalendar-weekDayLabel": {
-      fontWeight: 600,
-      color: "#004080",
-      textAlign: "center",
-      width: "calc(100% / 7)",
-      display: "inline-block",
-      margin: 0,
-      padding: 0,
-      lineHeight: 1,
-      fontSize: "0.7rem",
-    },
-    "& .MuiDayCalendar-weekContainer": {
-      marginTop: 0,
-      paddingTop: 0,
-    },
-    "& .MuiPickersDay-root": {
-      fontSize: "0.5rem",
-      width: "calc(100% / 7)",
-      height: 22,
-      margin: 0,
-      fontStyle: "normal",           // entfernt Kursivschrift
-      fontFamily: "Arial, sans-serif"
-    },
-  }}
->
-
+      <Box
+        sx={{
+          bgcolor: '#eff4f9ff',
+          borderRadius: 2,
+          boxShadow: 1,
+          p: 0.5,
+          '& .MuiDateCalendar-root': {
+            width: '100%',
+            maxHeight: 240,
+          },
+          '& .MuiPickersCalendarHeader-label': {
+            fontWeight: 'bold',
+            fontSize: '1rem',
+            textAlign: 'center',
+          },
+          '& .MuiDayCalendar-weekDayLabel': {
+            fontWeight: 600,
+            color: '#004080',
+            textAlign: 'center',
+            width: 'calc(100% / 7)',
+            display: 'inline-block',
+            margin: 0,
+            padding: 0,
+            lineHeight: 1,
+            fontSize: '0.7rem',
+          },
+          '& .MuiDayCalendar-weekContainer': {
+            marginTop: 0,
+            paddingTop: 0,
+          },
+          '& .MuiPickersDay-root': {
+            fontSize: '0.5rem',
+            width: 'calc(100% / 7)',
+            height: 22,
+            margin: 0,
+            fontStyle: 'normal', // entfernt Kursivschrift
+            fontFamily: 'Arial, sans-serif',
+          },
+        }}
+      >
         <DateCalendar
           value={date}
           onChange={onChange}
-          views={["day"]}
+          views={['day']}
           showDaysOutsideCurrentMonth
+          aria-label="Datum wählen"
           dayOfWeekFormatter={(date) =>
-            format(date, "EE", { locale: de }).toUpperCase()
+            format(date, 'EE', { locale: de }).toUpperCase()
           }
         />
       </Box>
     </LocalizationProvider>
   );
 }
-
-
-
