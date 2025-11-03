fc9d52455327419d1d4558a4f40041a9817c65a0
Julian <julian03.heine@web.de>
2025-11-03 16:04:27 +0100

    Was stupid

--- src/api/getToken.ts
+++ src/api/getToken.ts
@@ -1,39 +1,11 @@
-const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
-
-
+import useUser from '../../hooks/useUser';
 
 export class TokenService {
-    private token: string | undefined;
-    private tokenExpiry: number | undefined;
-
-    constructor() { }
 
     private async fetchToken(): Promise<string> {
-        if (this.token && this.tokenExpiry && Date.now() < this.tokenExpiry) {
-            return this.token;
-        }
-
-        const res = await fetch('https://keycloak.sau-portal.de/realms/sau/protocol/openid-connect/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                client_id: 'team-2',
-                client_secret: clientSecret,
-                grant_type: 'client_credentials',
-            }),
-        });
-
-        if (!res.ok) {
-            const errorText = await res.text();
-            throw new Error(`Token fetch failed: ${res.status} ${errorText}`);
-        }
-
-        const data = await res.json();
-        this.token = data.access_token;
-        this.tokenExpiry = Date.now() + data.expires_in * 1000 - 5000;
-        return this.token!;
+        const user = useUser();
+        const token = user.getAccessToken();
+        return token;
     }
 
     public async getToken(): Promise<string> {
