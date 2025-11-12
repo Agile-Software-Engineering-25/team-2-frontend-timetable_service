d08daa1161b958ddaf22b09573379b446685d3e3
Marcel Ahnert <marcel.ahnert@stud-provadis-hochschule.de>
2025-11-12 16:11:10 +0100

    probably fixed it - V2

--- src/@types/agile-shared-components.d.ts
+++ src/@types/agile-shared-components.d.ts
@@ -1,3 +1,5 @@
+import { createCustomJoyTheme } from '@agile-software/shared-components';
+import { createCustomMuiTheme } from '@agile-software/shared-components';
 declare module '@agile-software/shared-components' {
   export const createCustomTheme: (config: Record<string, unknown>) =>
     | { $$joy: Record<string, unknown> }
