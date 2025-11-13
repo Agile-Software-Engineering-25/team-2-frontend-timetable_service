9a0403a522f5358e873bd5133b16ee5c0172a16b
Janne <60547730+Janne6565@users.noreply.github.com>
2025-11-13 13:29:06 +0100

    Change base URL for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vitePluginSingleSpa from "vite-plugin-single-spa";
 import vitePluginReactHMR from "vite-plugin-react-single-spa-hmr";
 
 const PORT = parseInt(process.env.PORT ?? "5173");
-const BASE_URL_DEPLOYMENT = `http://localhost:${PORT}/`;
 
 const ENTRY_POINT = "src/singleSpa.tsx";
 
@@ -13,7 +12,7 @@ const NPM_EXTERNALS: string[] = [];
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : BASE_URL_DEPLOYMENT,
+  base: command === "serve" ? "/" : "api/ase-02/",
   plugins: [
     react(),
     command === "serve" && vitePluginReactHMR(ENTRY_POINT),
